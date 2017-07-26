0d48e3f566c7e24b09d9fa3f76558b180372dab8
Berni_Plays <bernhard.scharrer@gmx.net>
2017-07-26 08:31:03 +0200

    init commit

---
+++ src/guns/Guns.java
@@ -0,0 +1,17 @@
+package guns;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * 
+ * @author bernh
+ *
+ */
+public class Guns extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		super.onEnable();
+	}
+	
+}
